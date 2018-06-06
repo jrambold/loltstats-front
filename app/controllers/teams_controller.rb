c23bb34390fafb4f064b479173838a29b73abddc
jrambold <jrambold@gmail.com>
2018-06-06 11:46:47 -0600

    added testing files

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,6 +5,7 @@ class TeamsController < ApplicationController
 
   def show
     @team = Team.find(params[:id])
+    @thing = @team.best_by_position
     @best = TeamPresenter.new(@team.best_by_position).lanes
   end
 end
